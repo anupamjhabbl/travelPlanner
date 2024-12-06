da3daaa6df5d03d6b30acd1888d397be1eb0e3c9
anupamjhabbl <anupam.kumar@admo.astra.co.id>
2024-12-06 07:05:55 +0530

    Cleanup

--- app/src/main/java/com/example/tripplanner/MainActivity.kt
+++ app/src/main/java/com/example/tripplanner/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/example/tripplanner/common/composables/TitleBarView.kt
+++ app/src/main/java/com/example/tripplanner/common/composables/TitleBarView.kt
@@ -2,15 +2,12 @@ package com.example.tripplanner.common.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
