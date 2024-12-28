40553e5fd921c54dea00936e36a8acdda44122e3
anupamjhabbl <anupam.kumar@admo.astra.co.id>
2024-12-28 15:55:10 +0530

    Gradle Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tripplanner"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tripplanner"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 23
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
