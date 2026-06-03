0e4747beb4ef5899e5d55781e2582411049da3fb
anupamjhabbl <anupamimpbbl@gmail.com>
2026-06-04 00:15:26 +0530

    Update the add photo icon to CameraEnhance in TripGalleryScreen.

--- app/src/main/java/com/example/bbltripplanner/screens/userTrip/composables/TripGalleryScreen.kt
+++ app/src/main/java/com/example/bbltripplanner/screens/userTrip/composables/TripGalleryScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material.icons.filled.CameraEnhance
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -142,7 +143,7 @@ fun TripGalleryScreen(
                 modifier = Modifier.padding(bottom = 16.dp),
             ) {
                 Icon(
-                    imageVector = Icons.Default.Add,
+                    imageVector = Icons.Default.CameraEnhance,
                     contentDescription = null,
                     modifier = Modifier.size(24.dp),
                     tint = LocalCustomColors.current.primaryButtonText
